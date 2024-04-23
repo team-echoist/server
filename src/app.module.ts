f21aae859ca3b2c7742ea3bf0d540b629c0fcda3
JoDaeChan <daechan476@gmail.com>
2024-04-23 14:10:08 +0900

    fix: Update redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtInterceptor } from './common/interceptros/Jwt.interceptor';
 import { AuthModule } from './modules/auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+    CacheModule.register({
+      global: true,
+      store: redisStore,
+      url: process.env.REDIS_URL,
+    }),
+  ],
   providers: [{ provide: APP_INTERCEPTOR, useClass: JwtInterceptor }],
 })
 export class AppModule {}
