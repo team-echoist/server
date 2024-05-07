d06500be790e4ffcb34312ce28337afbc9f1a284
daechan_jo <103374153+daechan-jo@users.noreply.github.com>
2024-05-07 23:37:10 +0900

    fix: Update check email api (#67)
    * feat: Add get report list
    
    * feat: Add get essay with reports api
    
    * feat: Add Administrator report processing API
    
    - 주요 업데이트
    처분에 맞게 에세이 상태 업데이트 -> 에세이에 연결된 모든 리포트 상태 처리완료 동기화 -> 각 리포트에 대한 처리 내역 히스토리 생성
    
    - 에세이 관련 dto 및 로직 수정
    서버 로직 내부에서 categoryId 가 아닌 Category 를 참조하도록 변경
    
    * feat: Add Administrator report processing API
    
    - 주요 업데이트
    처분에 맞게 에세이 상태 업데이트 -> 에세이에 연결된 모든 리포트 상태 처리완료 동기화 -> 각 리포트에 대한 처리 내역 히스토리 생성
    
    - 에세이 관련 dto 및 로직 수정
    서버 로직 내부에서 categoryId 가 아닌 Category 를 참조하도록 변경
    
    * feat: Add Administrator waiting review list API
    
    - 주요 업데이트
    리뷰큐에 대기중인 리뷰 리스트 조회 api
    응답 dto 보안
    
    - 시드 데이터 추가
    샘플 유저, 에세이, 리뷰, 리포트
    
    * feat: Add Administrator waiting review list API
    
    - 주요 업데이트
    리뷰큐에 대기중인 리뷰 리스트 조회 api
    응답 dto 보안
    
    - 시드 데이터 추가
    샘플 유저, 에세이, 리뷰, 리포트
    
    * feat: Add root path
    
    * feat: Add root path
    
    * feat: Add root path
    
    * feat: Add root path
    
    * feat: Add root path
    
    * refactor: Update dto dir
    
    * feat: Add google oauth
    
    * fix: Update check email api

--- src/common/guards/strategies/google.strategy.ts
+++ src/common/guards/strategies/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CLIENT_CALLBACK,
       scope: ['email', 'profile'],
     });
   }
