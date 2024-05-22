783265012943ad0421d18fef6c378a964751540d
JoDaeChan <daechan476@gmail.com>
2024-05-23 02:03:29 +0900

    fix: Remove getRecommendEssays method from essay repository

--- src/modules/essay/essay.repository.ts
+++ src/modules/essay/essay.repository.ts
@@ -200,13 +200,4 @@ export class EssayRepository {
       .execute();
     return;
   }
-
-  async getRecommendEssays(limit: number) {
-    return this.essayRepository
-      .createQueryBuilder('essay')
-      .where('essay.published = :published', { published: true })
-      .orderBy('RANDOM()')
-      .limit(limit)
-      .getMany();
-  }
 }
