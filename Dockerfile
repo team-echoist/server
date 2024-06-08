9df7042eb246a5015b5f20f34672106fdf532eb0
JoDaeChan <daechan476@gmail.com>
2024-06-09 07:52:11 +0900

    refactor: Update typeorm configuration and exception handling logic

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,7 @@ RUN npm run build
 
 EXPOSE 3000
 
+COPY start.sh /app/start.sh
+RUN chmod +x /app/start.sh
+
 CMD ["/app/start.sh"]
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Running database migrations..."
-npm run migration:run
+#echo "Running database migrations..."
+#npm run migration:run
 
 echo "Starting the server..."
 npm run start:prod
