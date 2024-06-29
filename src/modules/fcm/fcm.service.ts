4ad52f02e12c7177debf996c52ec501a3d4e3852
JoDaeChan <daechan476@gmail.com>
2024-06-30 02:11:20 +0900

    fix: Update fcm admin setting

--- src/modules/fcm/fcm.service.ts
+++ src/modules/fcm/fcm.service.ts
@@ -1,29 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import { ConfigService } from '@nestjs/config';
+import { AwsService } from '../aws/aws.service';
 
 @Injectable()
 export class FcmService {
-  constructor(private readonly configService: ConfigService) {
-    const y = '외않되?';
-    const serviceAccount = {
-      type: 'service_account',
-      project_id: this.configService.get<string>('FIREBASE_PROJECT_ID'),
-      private_key_id: this.configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
-      private_key: this.configService.get<string>('FIREBASE_PRIVATE_KEY')?.replace(/\\n/g, '\n'),
-      client_email: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-      client_id: this.configService.get<string>('FIREBASE_CLIENT_ID'),
-      auth_uri: this.configService.get<string>('FIREBASE_AUTH_URI'),
-      token_uri: this.configService.get<string>('FIREBASE_TOKEN_URI'),
-      auth_provider_x509_cert_url: this.configService.get<string>(
-        'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
-      ),
-      client_x509_cert_url: this.configService.get<string>('FIREBASE_CLIENT_X509_CERT_URL'),
-    };
+  constructor(private readonly awsService: AwsService) {}
 
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    });
+  async onModuleInit() {
+    try {
+      const serviceAccount = await this.awsService.getServiceAccountKey();
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    } catch (error) {
+      console.error('Failed to load service account key file from S3:', error);
+      throw error;
+    }
   }
 
   async sendPushNotification(deviceToken: string, title: string, body: string) {
