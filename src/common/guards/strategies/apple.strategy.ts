584a408c83574011d2499606f316e26d505b703f
JoDaeChan <daechan476@gmail.com>
2024-07-28 12:31:32 +0900

    test: apple id token

--- src/common/guards/strategies/apple.strategy.ts
+++ src/common/guards/strategies/apple.strategy.ts
@@ -2,7 +2,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy, VerifyCallback } from 'passport-apple';
 import * as jwt from 'jsonwebtoken';
-import axios from 'axios';
 
 @Injectable()
 export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
@@ -18,65 +17,31 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
   }
 
   async validate(
-    req: any,
     accessToken: string,
     refreshToken: string,
-    idToken: string,
+    decodedIdToken: any,
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
     try {
-      const code = req.body.code;
-
-      const tokenResponse = await axios.post('https://appleid.apple.com/auth/token', null, {
-        params: {
-          grant_type: 'authorization_code',
-          code: code,
-          client_id: process.env.APPLE_CLIENT_ID,
-          client_secret: this.createClientSecret(),
-        },
-      });
-
-      const idTokenDecoded = jwt.decode(tokenResponse.data.id_token) as any;
-      console.log('Decoded ID Token:', idTokenDecoded);
-
-      if (!idTokenDecoded) {
-        throw new UnauthorizedException('Invalid idToken');
-      }
-
-      const { sub: id, email } = idTokenDecoded;
-      if (!id) {
-        throw new UnauthorizedException('No user ID returned from Apple');
-      }
-
-      console.log('흐엥: ', id, email);
-
+      console.log('accessToken: ', accessToken);
+      console.log('-----------------------------------');
+      console.log('-----------------------------------');
+      console.log('refreshToken: ', refreshToken);
+      console.log('-----------------------------------');
+      console.log('-----------------------------------');
+      console.log('decodedIdToken: ', decodedIdToken);
+      console.log('-----------------------------------');
+      console.log('-----------------------------------');
+      console.log('profile: ', profile);
       const user = {
         platform: 'apple',
-        platformId: id,
-        email: email || '',
+        platformId: decodedIdToken.sub,
+        email: profile.email || null,
       };
-
       done(null, user);
     } catch (err) {
       throw new UnauthorizedException();
     }
-  }
-
-  private createClientSecret() {
-    const claims = {
-      iss: process.env.APPLE_TEAM_ID,
-      aud: 'https://appleid.apple.com',
-      sub: process.env.APPLE_CLIENT_ID,
-      iat: Math.floor(Date.now() / 1000),
-      exp: Math.floor(Date.now() / 1000) + 3600,
-    };
-    return jwt.sign(claims, process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, '\n'), {
-      algorithm: 'ES256',
-      header: {
-        alg: 'ES256',
-        kid: process.env.APPLE_KEY_ID,
-      },
-    });
   }
 }
