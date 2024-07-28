62857b0bccde073baec87422bf2708ca3b89fe9d
JoDaeChan <daechan476@gmail.com>
2024-07-28 12:57:44 +0900

    test: apple id token

--- src/common/guards/strategies/apple.strategy.ts
+++ src/common/guards/strategies/apple.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy, VerifyCallback } from 'passport-apple';
 import * as jwt from 'jsonwebtoken';
+import axios from 'axios';
 
 @Injectable()
 export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
@@ -19,21 +20,24 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
   async validate(
     accessToken: string,
     refreshToken: string,
-    decodedIdToken: any,
+    idToken: string,
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
     try {
-      console.log('accessToken: ', accessToken);
-      console.log('-----------------------------------');
-      console.log('-----------------------------------');
-      console.log('refreshToken: ', refreshToken);
-      console.log('-----------------------------------');
-      console.log('-----------------------------------');
-      console.log('decodedIdToken: ', decodedIdToken);
-      console.log('-----------------------------------');
-      console.log('-----------------------------------');
-      console.log('profile: ', profile);
+      const appleKeys = await axios.get('https://appleid.apple.com/auth/keys');
+      const keys = appleKeys.data.keys;
+      const publicKey = keys[0];
+
+      console.log('애플키를 검증해봅시다');
+      console.log('애플 공개키: ', appleKeys);
+      console.log('애플 공개키들: ', keys);
+      console.log('애플 공개키들 중 첫번째: ', publicKey);
+
+      // 공개 키를 사용하여 ID 토큰 디코딩 및 검증
+      const decodedIdToken = jwt.verify(idToken, publicKey, { algorithms: ['RS256'] });
+      console.log('첫 번째 공개키로 디코딩한것', decodedIdToken);
+
       const user = {
         platform: 'apple',
         platformId: decodedIdToken.sub,
