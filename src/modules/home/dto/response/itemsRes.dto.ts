13bd3b90de6c8669badb73566967fefa019e50a5
JoDaeChan <daechan476@gmail.com>
2024-09-25 15:08:09 +0900

    doc: swagger update

--- src/modules/home/dto/response/itemsRes.dto.ts
+++ src/modules/home/dto/response/itemsRes.dto.ts
@@ -1,5 +1,7 @@
 import { ItemResDto } from './itemRes.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ItemsResDto {
+  @ApiProperty({ type: [ItemResDto] })
   items: ItemResDto[];
 }
--- src/modules/home/dto/response/themesRes.dto.ts
+++ src/modules/home/dto/response/themesRes.dto.ts
@@ -1,5 +1,7 @@
 import { ThemeResDto } from './themeRes.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ThemesResDto {
+  @ApiProperty({ type: [ThemeResDto] })
   themes: ThemeResDto[];
 }
