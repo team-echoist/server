0239d167fa1554f74afbfe9d0218401b3a6933a2
daechan_jo <103374153+daechan-jo@users.noreply.github.com>
2024-11-01 00:07:11 +0900

    fix: 외않되진쨔재뱔한번마 (#468)
    * refactor: update cron task names and add key-based filtering in getCronLogs
    
    - getCronLogs 메소드에 key 파라미터를 추가하여 taskName을 기준으로 필터링 가능하도록 수정
    - Cron 작업의 로그 태스크 이름을 간결하게 변경
    
    * refactor: update cron task names and add key-based filtering in getCronLogs
    
    - getCronLogs 메소드에 key 파라미터를 추가하여 taskName을 기준으로 필터링 가능하도록 수정
    - Cron 작업의 로그 태스크 이름을 간결하게 변경
    
    * feat: store database implementation
    
    * feat: theme crud
    
    * feat: item crud
    
    * feat: view and purchase themes
    
    * feat: view and purchase items
    
    * feat: activate themes and items
    
    * feat: Add layout information to user basic information and modify caching
    
    * doc: swagger update
    
    * doc: swagger update
    
    * doc: swagger update
    
    * fix: problem with published posts not being viewed properly due to paging issues
    
    * test: Added console log to resolve issues that occur when using rt
    
    * test: Added console log to resolve issues that occur when using rt
    
    * feat: middleware is added to add request request identifier for testing.
    
    * feat: middleware is added to add request request identifier for testing.
    
    * fix: replace app.enableCors with express cors middleware for custom headers
    
    * fix: replace app.enableCors with express cors middleware for custom headers
    
    * fix: add X-Access-Token to Access-Control-Expose-Headers
    
    * feat: Add basic theme creation logic
    
    * refactor: guard refactoring and improving duplicate handling logic
    
    * fix: remove tag limit
    
    * refactor: item and theme purchase logic by applying Redis distributed locks and transactions
    
    * refactor: Add retry logic to method
    
    * refactor: Added name duplication check logic when saving and updating stories
    
    * fix: 키워드 타입 검사 추가
    
    * fix: 키워드 타입 검사 추가
    
    * chore: Update dependency package versions to latest
    
    * chore: 의존성 패키지 업데이트
    
    * chore: 의존성 패키지 업데이트
    
    * chore: 의존성 패키지 업데이트
    
    * chore: eslint updated
    
    * fix: redis connection settings in redlock
    
    * fix: 에세이 조회 분산락에 지수 백오프 적용
    
    * doc: edit readme
    
    * chore: edit eslint config
    
    * fix: 땅에 묻기 기능 추가로 인한 기존 로직 변경
    
    * fix: 땅에묻기 필수 좌표 분기 추가
    
    * refactor: geometry 타입 변경 밑작업
    
    * fix: 에세이 작성시 위도,경도 데이터 geometry로 변환
    
    * feat: 사용자 위치 기반 주변 에세이 알림 추가
    
    * feat: 사용자 위치 기반 주변 에세이 알림 추가
    
    * fix: 장치 설정 검색을 일괄 처리하여 성능 최적화
    
    * fix: 관리자 검색 API에 대한 페이지네이션 추가
    
    * feat: 유저 및 에세이 유사도 검색 추가
    
    * feat: 사용자 위치 기반 주변 에세이 리스트 조회
    
    * refactor: 에세이 상세조회 api에 위치기반 에세이 조회 페이지 기능 추가
    
    * test: alert service 테스트 코드 개선
    
    * test: auth service 테스트 코드 개선
    
    * test: badge service 테스트 코드 개선
    
    * test: bookmark service 테스트 코드 개선
    
    * test: burial service 테스트 코드 개선
    
    * test: essay service 테스트 코드 개선
    
    * fix: 에세이 상태 타입 리네임
    
    * draft: essay 테스트코드 개선
    
    * fix: 스토리 생성 후 응답본문 추가
    
    * fix: 마이그레이션 수정
    
    * fix: 마이그레이션 수정
    
    * fix: 마이그레이션 수정
    
    * fix: 마이그레이션 수정
    
    * fix: 마이그레이션 수정
    
    * fix: 마이그레이션 수정

--- src/migrations/2024/10/1730304392337-ChangePublicType.ts
+++ src/migrations/2024/10/1730304392337-ChangePublicType.ts
@@ -4,7 +4,7 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
   name = 'ChangePublishedToPublic1730304392337';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // 1. 새로운 enum 타입을 미리 생성
+    // 1. 새 enum 타입 생성 및 컬럼에 적용
     await queryRunner.query(
       `CREATE TYPE "public"."new_review_queue_type_enum" AS ENUM('linkedout', 'public', 'burial')`,
     );
@@ -15,10 +15,10 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
       `CREATE TYPE "public"."new_alert_type_enum" AS ENUM('public', 'linkedout', 'support')`,
     );
     await queryRunner.query(
-      `CREATE TYPE "public"."new_essay_status_enum" AS ENUM('private', 'public')`,
+      `CREATE TYPE "public"."new_essay_status_enum" AS ENUM('private', 'published', 'public', 'linkedout', 'burial')`,
     );
 
-    // 2. 기존 `published` 및 `unpublished` 데이터를 임시 값으로 업데이트
+    // 2. `published`를 `public`으로 업데이트
     await queryRunner.query(
       `UPDATE "review_queue" SET "type" = 'public' WHERE "type" = 'published'`,
     );
@@ -26,22 +26,19 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
       `UPDATE "processed_history" SET "action_type" = 'public' WHERE "action_type" = 'published'`,
     );
     await queryRunner.query(`UPDATE "alert" SET "type" = 'public' WHERE "type" = 'published'`);
-    await queryRunner.query(
-      `UPDATE "processed_history" SET "action_type" = 'unpublic' WHERE "action_type" = 'unpublished'`,
-    );
 
     // 3. 새 enum 타입으로 컬럼 변경
     await queryRunner.query(
-      `ALTER TABLE "review_queue" ALTER COLUMN "type" TYPE "public"."new_review_queue_type_enum" USING "type"::"text"::"public"."new_review_queue_type_enum"`,
+      `ALTER TABLE "review_queue" ALTER COLUMN "type" TYPE "public"."new_review_queue_type_enum" USING "type"::text::"public"."new_review_queue_type_enum"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "processed_history" ALTER COLUMN "action_type" TYPE "public"."new_processed_history_action_type_enum" USING "action_type"::"text"::"public"."new_processed_history_action_type_enum"`,
+      `ALTER TABLE "processed_history" ALTER COLUMN "action_type" TYPE "public"."new_processed_history_action_type_enum" USING "action_type"::text::"public"."new_processed_history_action_type_enum"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "alert" ALTER COLUMN "type" TYPE "public"."new_alert_type_enum" USING "type"::"text"::"public"."new_alert_type_enum"`,
+      `ALTER TABLE "alert" ALTER COLUMN "type" TYPE "public"."new_alert_type_enum" USING "type"::text::"public"."new_alert_type_enum"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "essay" ALTER COLUMN "status" TYPE "public"."new_essay_status_enum" USING "status"::"text"::"public"."new_essay_status_enum"`,
+      `ALTER TABLE "essay" ALTER COLUMN "status" TYPE "public"."new_essay_status_enum" USING "status"::text::"public"."new_essay_status_enum"`,
     );
 
     // 4. 기존 enum 타입 삭제 후 새 enum 타입 이름 변경
@@ -67,7 +64,7 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // 1. 기존 enum 타입을 복원하기 위해 새로운 enum 타입 생성
+    // down: 원래 enum 타입 복원
     await queryRunner.query(
       `CREATE TYPE "public"."old_review_queue_type_enum" AS ENUM('linkedout', 'published', 'burial')`,
     );
@@ -77,9 +74,11 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
     await queryRunner.query(
       `CREATE TYPE "public"."old_alert_type_enum" AS ENUM('published', 'linkedout', 'support')`,
     );
-    await queryRunner.query(`CREATE TYPE "public"."old_essay_status_enum" AS ENUM('private')`);
+    await queryRunner.query(
+      `CREATE TYPE "public"."old_essay_status_enum" AS ENUM('private', 'published')`,
+    );
 
-    // 2. 데이터 원래대로 복원
+    // 데이터 복원
     await queryRunner.query(
       `UPDATE "review_queue" SET "type" = 'published' WHERE "type" = 'public'`,
     );
@@ -87,25 +86,22 @@ export class ChangePublishedToPublic1730304392337 implements MigrationInterface 
       `UPDATE "processed_history" SET "action_type" = 'published' WHERE "action_type" = 'public'`,
     );
     await queryRunner.query(`UPDATE "alert" SET "type" = 'published' WHERE "type" = 'public'`);
+
+    // 기존 enum 타입으로 컬럼 복원
     await queryRunner.query(
-      `UPDATE "processed_history" SET "action_type" = 'unpublished' WHERE "action_type" = 'unpublic'`,
+      `ALTER TABLE "review_queue" ALTER COLUMN "type" TYPE "public"."old_review_queue_type_enum" USING "type"::text::"public"."old_review_queue_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "processed_history" ALTER COLUMN "action_type" TYPE "public"."old_processed_history_action_type_enum" USING "action_type"::text::"public"."old_processed_history_action_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "alert" ALTER COLUMN "type" TYPE "public"."old_alert_type_enum" USING "type"::text::"public"."old_alert_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "essay" ALTER COLUMN "status" TYPE "public"."old_essay_status_enum" USING "status"::text::"public"."old_essay_status_enum"`,
     );
 
-    // 3. 기존 enum 타입으로 컬럼 복원
-    await queryRunner.query(
-      `ALTER TABLE "review_queue" ALTER COLUMN "type" TYPE "public"."old_review_queue_type_enum" USING "type"::"text"::"public"."old_review_queue_type_enum"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "processed_history" ALTER COLUMN "action_type" TYPE "public"."old_processed_history_action_type_enum" USING "action_type"::"text"::"public"."old_processed_history_action_type_enum"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "alert" ALTER COLUMN "type" TYPE "public"."old_alert_type_enum" USING "type"::"text"::"public"."old_alert_type_enum"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "essay" ALTER COLUMN "status" TYPE "public"."old_essay_status_enum" USING "status"::"text"::"public"."old_essay_status_enum"`,
-    );
-
-    // 4. 새 enum 타입 삭제 후 기존 enum 타입 이름 복원
+    // 새 enum 타입 삭제 후 기존 enum 타입 이름 복원
     await queryRunner.query(`DROP TYPE "public"."review_queue_type_enum"`);
     await queryRunner.query(
       `ALTER TYPE "public"."old_review_queue_type_enum" RENAME TO "review_queue_type_enum"`,
