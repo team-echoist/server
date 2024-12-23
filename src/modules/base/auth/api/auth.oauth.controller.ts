1828c8db2ca54ef4fe138bf61b85e61ef462941a
JoDaeChan <daechan476@gmail.com>
2024-12-23 15:24:20 +0900

    test: 일렉트론 oauth 리디렉션 테스트

--- src/modules/base/auth/api/auth.oauth.controller.ts
+++ src/modules/base/auth/api/auth.oauth.controller.ts
@@ -62,9 +62,8 @@ export class AuthOauthController {
 
     // let redirectUrl = this.configService.get<string>('WEB_REGISTER_REDIRECT');
     // redirectUrl += `?accessToken=${jwt.accessToken}&refreshToken=${jwt.refreshToken}`;
-    // res.redirect(redirectUrl);
-
-    res.json({ accessToken: jwt.accessToken, refreshToken: jwt.refreshToken });
+    const redirectUrl = `app://./web/login?accessToken=${jwt.accessToken}&refreshToken=${jwt.refreshToken}`;
+    res.redirect(redirectUrl);
   }
 
   @Post('google/mobile')
